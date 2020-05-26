6be5078817b23c09859cea511e8e516efb3ab190
himanshu1510 <64575592+himanshu1510@users.noreply.github.com>
2020-05-26 14:35:26 +0530

    Update disease2.py

--- disease2.py
+++ disease2.py
@@ -144,12 +144,12 @@ train_datagen = ImageDataGenerator(
         horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1./255)
 training_set = train_datagen.flow_from_directory(
-        '/root/ngrok-stable-linux-amd64(1)/project/cell_images/malaria/cell_image_train/',
+        '/root/ngrok-stable-linux-amd64 (1)/project/cell_images/malaria/cell_image_train/',
         target_size=(64, 64),
         batch_size=6,
         class_mode='binary')
 test_set = test_datagen.flow_from_directory(
-        '/root/ngrok-stable-linux-amd64(1)/project/cell_images/malaria/cell_image_test/',
+        '/root/ngrok-stable-linux-amd64 (1)/project/cell_images/malaria/cell_image_test/',
         target_size=(64, 64),
         batch_size=6,
         class_mode='binary')
